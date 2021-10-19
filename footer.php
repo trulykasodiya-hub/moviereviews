2f5389c766d219755fab5024db0595910898078c
prajeshkasodiya <patelprajesh232@gmail.com>
2021-10-19 15:40:29 +0530

    footer and header changes

---
+++ footer.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Amin Template">
+    <meta name="keywords" content="Amin, unica, creative, html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Movie Reviews</title>
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700,800,900&display=swap"
+        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Cinzel:400,700,900&display=swap" rel="stylesheet">
+
+    <!-- Css Styles -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="css/elegant-icons.css" type="text/css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="css/barfiller.css" type="text/css">
+    <link rel="stylesheet" href="css/magnific-popup.css" type="text/css">
+    <link rel="stylesheet" href="css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+
+
+<body>
+
+  <!-- Footer Section Begin -->
+  <footer class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="footer-about">
+                        <div class="fa-logo">
+                            <a href="#"><img src="img/f-logo.png" alt=""></a>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua lacus vel facilisis.</p>
+                        <div class="fa-social">
+                            <a href="#"><i class="fa fa-facebook"></i></a>
+                            <a href="#"><i class="fa fa-twitter"></i></a>
+                            <a href="#"><i class="fa fa-youtube-play"></i></a>
+                            <a href="#"><i class="fa fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="editor-choice">
+                        <div class="section-title">
+                            <h5>Editor's Choice</h5>
+                        </div>
+                        <div class="ec-item">
+                            <div class="ec-pic">
+                                <img src="img/trending/editor-1.jpg" alt="">
+                            </div>
+                            <div class="ec-text">
+                                <h6><a href="#">A Monster Prom poster got hijacked for a Papa Roach concert...</a>
+                                </h6>
+                                <ul>
+                                    <li><i class="fa fa-clock-o"></i> Aug 01, 2019</li>
+                                    <li><i class="fa fa-comment-o"></i> 12</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="ec-item">
+                            <div class="ec-pic">
+                                <img src="img/trending/editor-2.jpg" alt="">
+                            </div>
+                            <div class="ec-text">
+                                <h6><a href="#">Facebook wants to read your thoughts with its augmented...</a>
+                                </h6>
+                                <ul>
+                                    <li><i class="fa fa-clock-o"></i> Aug 01, 2019</li>
+                                    <li><i class="fa fa-comment-o"></i> 12</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="tags-cloud">
+                        <div class="section-title">
+                            <h5>Tags Cloud</h5>
+                        </div>
+                        <div class="tag-list">
+                            <a href="#"><span>Gaming</span></a>
+                            <a href="#"><span>Platform</span></a>
+                            <a href="#"><span>Playstation</span></a>
+                            <a href="#"><span>Hardware</span></a>
+                            <a href="#"><span>Reviews</span></a>
+                            <a href="#"><span>Simulation</span></a>
+                            <a href="#"><span>Strategy</span></a>
+                            <a href="#"><span>Scientific</span></a>
+                            <a href="#"><span>References</span></a>
+                            <a href="#"><span>Role-playing</span></a>
+                            <a href="#"><span>Adventurea</span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- Footer Section End -->
+    
+       <!-- Js Plugins -->
+       <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/circle-progress.min.js"></script>
+    <script src="js/jquery.barfiller.js"></script>
+    <script src="js/jquery.slicknav.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Amin Template">
+    <meta name="keywords" content="Amin, unica, creative, html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Movie Reviews</title>
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700,800,900&display=swap"
+        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Cinzel:400,700,900&display=swap" rel="stylesheet">
+
+    <!-- Css Styles -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="css/elegant-icons.css" type="text/css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="css/barfiller.css" type="text/css">
+    <link rel="stylesheet" href="css/magnific-popup.css" type="text/css">
+    <link rel="stylesheet" href="css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+
+<body>
+    <!-- Header Section Begin -->
+    <header class="header-section">
+        <div class="ht-options">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 col-md-8">
+                        <div class="ht-widget">
+                            <ul>
+                                <li><i class="fa fa-sun-o"></i> <span>20<sup>c</sup></span> London</li>
+                                <li><i class="fa fa-clock-o"></i> Aug 01, 2019</li>
+                                <li class="signup-switch signup-open"><i class="fa fa-sign-out"></i> Login / Sign up
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-4">
+                        <div class="ht-social">
+                            <a href="#"><i class="fa fa-facebook"></i></a>
+                            <a href="#"><i class="fa fa-twitter"></i></a>
+                            <a href="#"><i class="fa fa-youtube-play"></i></a>
+                            <a href="#"><i class="fa fa-instagram"></i></a>
+                            <a href="#"><i class="fa fa-envelope-o"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="logo">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+                       <a href="./index.html"><img src="img/logo.png" alt=""></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="nav-options">
+            <div class="container">
+                <div class="humberger-menu humberger-open">
+                    <i class="fa fa-bars"></i>
+                </div>
+                <div class="nav-search search-switch">
+                    <i class="fa fa-search"></i>
+                </div>
+                <div class="nav-menu">
+                    <ul>
+                        <li class="active"><a href="./index.html"><span>Home</span></a></li>
+                        <li class="mega-menu"><a href="#"><span>Platform <i class="fa fa-angle-down"></i></span></a>
+                            <div class="megamenu-wrapper">
+                                <ul class="mw-nav">
+                                    <li><a href="#"><span>Playstation</span></a></li>
+                                    <li><a href="#"><span>Windows</span></a></li>
+                                    <li><a href="#"><span>Mobile</span></a></li>
+                                    <li><a href="#"><span>Xbox</span></a></li>
+                                </ul>
+                                <div class="mw-post">
+                                    <div class="mw-post-item">
+                                        <div class="mw-pic">
+                                            <img src="img/megamenu/mm-1.jpg" alt="">
+                                        </div>
+                                        <div class="mw-text">
+                                            <h6><a href="#">A Monster Prom poster got hijacked for a Papa Roach
+                                                    concert...</a></h6>
+                                            <ul>
+                                                <li><i class="fa fa-clock-o"></i> Aug 01, 2019</li>
+                                                <li><i class="fa fa-comment-o"></i> 12</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="mw-post-item">
+                                        <div class="mw-pic">
+                                            <img src="img/megamenu/mm-2.jpg" alt="">
+                                        </div>
+                                        <div class="mw-text">
+                                            <h6><a href="#">A new Borderlands 3 trailer introduces Moze and her...</a>
+                                            </h6>
+                                            <ul>
+                                                <li><i class="fa fa-clock-o"></i> Aug 01, 2019</li>
+                                                <li><i class="fa fa-comment-o"></i> 12</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="mw-post-item">
+                                        <div class="mw-pic">
+                                            <img src="img/megamenu/mm-3.jpg" alt="">
+                                        </div>
+                                        <div class="mw-text">
+                                            <h6><a href="#">Teamfight Tactics is in chaos after today's patch...</a>
+                                            </h6>
+                                            <ul>
+                                                <li><i class="fa fa-clock-o"></i> Aug 01, 2019</li>
+                                                <li><i class="fa fa-comment-o"></i> 12</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="mw-post-item">
+                                        <div class="mw-pic">
+                                            <img src="img/megamenu/mm-4.jpg" alt="">
+                                        </div>
+                                        <div class="mw-text">
+                                            <h6><a href="#">Borderlands 2 dev explains why there's mysterious
+                                                    boxes...</a></h6>
+                                            <ul>
+                                                <li><i class="fa fa-clock-o"></i> Aug 01, 2019</li>
+                                                <li><i class="fa fa-comment-o"></i> 12</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="mw-post-item">
+                                        <div class="mw-pic">
+                                            <img src="img/megamenu/mm-5.jpg" alt="">
+                                        </div>
+                                        <div class="mw-text">
+                                            <h6><a href="#">Capcom asks select fans to test new Resident Evil game</a>
+                                            </h6>
+                                            <ul>
+                                                <li><i class="fa fa-clock-o"></i> Aug 01, 2019</li>
+                                                <li><i class="fa fa-comment-o"></i> 12</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="#"><span>Pages <i class="fa fa-angle-down"></i></span></a>
+                            <ul class="dropdown">
+                                <li><a href="./categories-list.html">Categories</a></li>
+                                <li><a href="./categories-grid.html">Categories grid</a></li>
+                                <li><a href="./typography.html">Typography</a></li>
+                                <li><a href="./details-post-default.html">Post default</a></li>
+                                <li><a href="./details-post-gallery.html">Post gallery</a></li>
+                                <li><a href="./details-post-review.html">Post review</a></li>
+                                <li><a href="./contact.html">Contact</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#"><span>Reviews <i class="fa fa-angle-down"></i></span></a></li>
+                        <li><a href="#"><span>Windows <i class="fa fa-angle-down"></i></span></a></li>
+                        <li><a href="#"><span>Videos <i class="fa fa-angle-down"></i></span></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Header End -->
+    
+       <!-- Js Plugins -->
+       <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/circle-progress.min.js"></script>
+    <script src="js/jquery.barfiller.js"></script>
+    <script src="js/jquery.slicknav.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
