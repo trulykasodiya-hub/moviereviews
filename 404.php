3836b3f0e8ed931214e81224a0fd51c9f0990da9
prajeshkasodiya <patelprajesh232@gmail.com>
2021-10-20 12:38:04 +0530

    header changes

--- 404.php
+++ 404.php
@@ -26,74 +26,7 @@
 </head>
 
 <body>
-    <!-- Page Preloder -->
-    <div id="preloder">
-        <div class="loader"></div>
-    </div>
-
-    <!-- Humberger Menu Begin -->
-    <div class="humberger-menu-overlay"></div>
-    <div class="humberger-menu-wrapper">
-        <div class="hw-logo">
-            <a href="#"><img src="img/f-logo.png" alt=""></a>
-        </div>
-        <div class="hw-menu mobile-menu">
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Platform <i class="fa fa-angle-down"></i></a>
-                    <ul class="dropdown">
-                        <li><a href="#">Playstation</a></li>
-                        <li><a href="#">Windows</a></li>
-                        <li><a href="#">Mobile</a></li>
-                        <li><a href="#">Xbox</a></li>
-                    </ul>
-                </li>
-                <li><a href="#">Reviews <i class="fa fa-angle-down"></i></a>
-                    <ul class="dropdown">
-                        <li><a href="#">Playstation</a></li>
-                        <li><a href="#">Windows</a></li>
-                        <li><a href="#">Mobile</a></li>
-                        <li><a href="#">Xbox</a></li>
-                    </ul>
-                </li>
-                <li><a href="#">Windows <i class="fa fa-angle-down"></i></a>
-                    <ul class="dropdown">
-                        <li><a href="#">Playstation</a></li>
-                        <li><a href="#">Windows</a></li>
-                        <li><a href="#">Mobile</a></li>
-                        <li><a href="#">Xbox</a></li>
-                    </ul>
-                </li>
-                <li><a href="#">Videos <i class="fa fa-angle-down"></i></a>
-                    <ul class="dropdown">
-                        <li><a href="#">Playstation</a></li>
-                        <li><a href="#">Windows</a></li>
-                        <li><a href="#">Mobile</a></li>
-                        <li><a href="#">Xbox</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
-        <div id="mobile-menu-wrap"></div>
-        <div class="hw-social">
-            <a href="#"><i class="fa fa-facebook"></i></a>
-            <a href="#"><i class="fa fa-twitter"></i></a>
-            <a href="#"><i class="fa fa-youtube-play"></i></a>
-            <a href="#"><i class="fa fa-instagram"></i></a>
-        </div>
-        <div class="hw-insta-media">
-            <div class="section-title">
-                <h5>Instagram</h5>
-            </div>
-            <div class="insta-pic">
-                <img src="img/instagram/ip-1.jpg" alt="">
-                <img src="img/instagram/ip-2.jpg" alt="">
-                <img src="img/instagram/ip-3.jpg" alt="">
-                <img src="img/instagram/ip-4.jpg" alt="">
-            </div>
-        </div>
-    </div>
-    <!-- Humberger Menu End -->
+  
 
     <?php  include "header.php" ?>
 
