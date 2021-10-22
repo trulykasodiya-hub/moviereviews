e0848c01d1d3c42e29872969eb7d8e260e47387c
prajeshkasodiya <patelprajesh232@gmail.com>
2021-10-22 18:51:52 +0530

    file exatantion change

--- header.php
+++ header.php
@@ -131,7 +131,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-lg-12 text-center">
-                       <a href="./index.html"><img src="img/logo.png" alt=""></a>
+                       <a href="./index.php"><img src="img/logo.png" alt=""></a>
                     </div>
                 </div>
             </div>
@@ -146,7 +146,7 @@
                 </div>
                 <div class="nav-menu">
                     <ul>
-                        <li class="active"><a href="./index.html"><span>Home</span></a></li>
+                        <li class="active"><a href="./index.php"><span>Home</span></a></li>
                         <li class="mega-menu"><a href="#"><span>Platform <i class="fa fa-angle-down"></i></span></a>
                             <div class="megamenu-wrapper">
                                 <ul class="mw-nav">
@@ -227,13 +227,13 @@
                         <li>
                             <a href="#"><span>Pages <i class="fa fa-angle-down"></i></span></a>
                             <ul class="dropdown">
-                                <li><a href="./categories-list.html">Categories</a></li>
-                                <li><a href="./categories-grid.html">Categories grid</a></li>
-                                <li><a href="./typography.html">Typography</a></li>
-                                <li><a href="./details-post-default.html">Post default</a></li>
-                                <li><a href="./details-post-gallery.html">Post gallery</a></li>
-                                <li><a href="./details-post-review.html">Post review</a></li>
-                                <li><a href="./contact.html">Contact</a></li>
+                                <li><a href="./categories-list.php">Categories</a></li>
+                                <li><a href="./categories-grid.php">Categories grid</a></li>
+                                <li><a href="./typography.php">Typography</a></li>
+                                <li><a href="./details-post-default.php">Post default</a></li>
+                                <li><a href="./details-post-gallery.php">Post gallery</a></li>
+                                <li><a href="./details-post-review.php">Post review</a></li>
+                                <li><a href="./contact.php">Contact</a></li>
                             </ul>
                         </li>
                         <li><a href="#"><span>Reviews <i class="fa fa-angle-down"></i></span></a></li>
